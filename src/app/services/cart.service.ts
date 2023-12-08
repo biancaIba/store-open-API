efa3cce1ef9d0ba858daef17d654fe7ce4c2343c
Bianca Ibañez <biancaiba11@gmail.com>
2023-12-08 15:21:51 -0300

    feat: get cart from storage

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { Product } from '../interfaces/products.interface';
+import { ProductsService } from './products.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
-
   private cartChangesSubject: Subject<void> = new Subject<void>();
 
-  constructor() {}
+  constructor(private productsService: ProductsService) {}
 
   addProduct(productId: number): void {
     const id = productId.toString();
@@ -45,5 +46,35 @@ export class CartService {
 
   obtainCartChanges() {
     return this.cartChangesSubject.asObservable();
+  }
+
+  getProductsFromStorage(): Product[] {
+    const products: Product[] = [];
+    for (let i = 0; i < sessionStorage.length; i++) {
+      const key = sessionStorage.key(i);
+      let product: Product = null;
+      if (key) {
+        this.productsService
+          .getProductById(parseInt(key))
+          .subscribe((res: Product) => {
+            product = res;
+            products.push(product);
+          });
+      }
+    }
+    return products;
+  }
+
+  getQuantitysFromStorage(): number[] {
+    const quantities: number[] = [];
+    for (let i = 0; i < sessionStorage.length; i++) {
+      const key = sessionStorage.key(i);
+      let quantity: number = null;
+      if (key) {
+        quantity = parseInt(sessionStorage.getItem(key));
+        quantities.push(quantity);
+      }
+    }
+    return quantities;
   }
 }
