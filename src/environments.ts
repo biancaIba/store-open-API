3c32c7ce4d44e376972c99cc84628c06f1db31d0
Bianca Ibañez <biancaiba11@gmail.com>
2023-12-08 12:27:20 -0300

    feat: firebase config

--- src/environments.ts
+++ src/environments.ts
@@ -1,4 +1,12 @@
 export const env = {
-    url: 'https://fakestoreapi.com/',
-    site: 'http://localhost:3055/menu/',
+  production: false,
+  url: 'https://fakestoreapi.com/',
+  firebaseConfig: {
+    apiKey: 'AIzaSyDU5UpzqVpt8dood0pXcLijD5upgAJ9K_U',
+    authDomain: 'final-project-dbe5a.firebaseapp.com',
+    projectId: 'final-project-dbe5a',
+    storageBucket: 'final-project-dbe5a.appspot.com',
+    messagingSenderId: '305588239589',
+    appId: '1:305588239589:web:c284930a578a798653e80c',
+  },
 };
