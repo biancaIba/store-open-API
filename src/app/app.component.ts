37ab4670ec79aceae54c6872182e9497bd1894a4
Bianca Ibañez <biancaiba11@gmail.com>
2023-12-08 11:52:17 -0300

    feat: add "hasCart"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from './services/cart.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 
-export class AppComponent {
-  title = 'Final Project';
+export class AppComponent implements OnInit {
+
+  title: string = 'Final Project';
+  hasCart: boolean = null;
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.hasCart = this.cartService.hasProducts();
+  }
+
 }
