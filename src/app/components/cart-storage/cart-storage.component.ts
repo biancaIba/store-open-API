88d92a37c9cfd82f3fe15920fdab0a34e41bd098
Bianca Ibañez <biancaiba11@gmail.com>
2023-12-14 20:55:03 -0300

    feat: pay

--- src/app/components/cart-storage/cart-storage.component.ts
+++ src/app/components/cart-storage/cart-storage.component.ts
@@ -1,21 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/interfaces/products.interface';
 import { CartService } from '../../services/cart.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-cart-storage',
   templateUrl: './cart-storage.component.html',
-  styleUrls: ['./cart-storage.component.css']
+  styleUrls: ['./cart-storage.component.css'],
 })
-export class CartStorageComponent implements OnInit{
-
+export class CartStorageComponent implements OnInit {
   products: Product[] = [];
   quantities: number[] = [];
+  logged: boolean = false;
+  message: string = '';
 
-  constructor(private cartService: CartService) {}
+  constructor(
+    private cartService: CartService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.products = this.cartService.getProductsFromStorage();
     this.quantities = this.cartService.getQuantitysFromStorage();
+    this.authService.getUserData().subscribe((res: any) => {
+      if (res) this.logged = true;
+    });
+  }
+
+  pay(): void {
+    if (this.logged) this.message = 'Thanks for buying with us!';
+    else this.message = 'You must login to buy';
   }
 }
